a74371d64fee86940ab4e94a1004d0a506384906
Hitesh Tare <hiteshtare@gmail.com>
2020-04-23 12:40:07 +0530

    OMNI-6101: Dynamic circles rendering from config

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-//================================CIRCLE A================================//
-//Config  for Circle A - Nodes Count
-const circleA_config = Object({
+//================================Load JSON data================================//
+$(function () {
+  $.getJSON('./data/mock.json', function (data) {
+    console.log('JSON loaded :)');
+    const Customer_Level = data.Customer_Level;
+    const Segment_Level = data.Segment_Level;
+
+  }).error(function () {
+    console.log('Unable to load JSON :(');
+  });
+});
+//================================Load JSON data================================//
+
+const circles_config = [{
   Meeting: 5,
   Facebook: 10,
   Email: 10,
@@ -15,26 +26,7 @@ const circleA_config = Object({
   nodeOriginX: 0,
   nodeOriginY: 0,
   totalNodes: 0
-});
-//Calculate raduis and Node Origin for Circle A
-calculateRadius_NodeOrigin(circleA_config);
-//Container for Circle A
-const circleA_svg = renderCircle(circleA_config);
-
-renderAllNodes(circleA_svg, circleA_config);
-
-//To render graph legends
-// renderLegends(circleA_svg, circleA_config);
-
-//To handles sliders for Circle A
-rangeSliderForNodes(circleA_svg, circleA_config);
-
-//To add click event for all Nodes to navigate for Circle A
-addClickEventsforNodes(circleA_config);
-//================================CIRCLE A================================//
-//================================CIRCLE B================================//
-//Config  for Circle B - Nodes Count
-const circleB_config = Object({
+}, {
   Meeting: 4,
   Facebook: 7,
   Email: 7,
@@ -49,18 +41,7 @@ const circleB_config = Object({
   nodeOriginX: 0,
   nodeOriginY: 0,
   totalNodes: 0
-});
-//Calculate raduis and Node Origin for Circle B
-calculateRadius_NodeOrigin(circleB_config);
-//Container for Circle B
-const circleB_svg = renderCircle(circleB_config);
-
-renderAllNodes(circleB_svg, circleB_config);
-//================================CIRCLE B================================//
-
-//================================CIRCLE C================================//
-//Config for Circle C - Nodes Count
-const circleC_config = Object({
+}, {
   Meeting: 2,
   Facebook: 5,
   Email: 5,
@@ -75,11 +56,38 @@ const circleC_config = Object({
   nodeOriginX: 0,
   nodeOriginY: 0,
   totalNodes: 0
-});
-//Calculate raduis and Node Origin for Circle C
-calculateRadius_NodeOrigin(circleC_config);
-//Container for Circle C
-const circleC_svg = renderCircle(circleC_config);
+}, {
+  Meeting: 1,
+  Facebook: 3,
+  Email: 3,
+  SMS: 2,
+  Text: 'Segment D',
+  id: 'circleD',
+  url: 'https://observablehq.com/@d3/zoomable-circle-packing',
+  originX: 300,
+  originY: 300,
+  //Below values are Calculated
+  raduis: 0,
+  nodeOriginX: 0,
+  nodeOriginY: 0,
+  totalNodes: 0
+}];
 
-renderAllNodes(circleC_svg, circleC_config);
-//================================CIRCLE C================================//
+circles_config.forEach((circle_config, index) => {
+  //Calculate raduis and Node Origin for Circle A
+
+  calculateRadius_NodeOrigin(circle_config);
+  //Container for Circle A
+  const circle_svg = renderCircle(circle_config);
+
+  renderAllNodes(circle_svg, circle_config);
+
+  //For index 0 i.e. first Circles render slider and add click for navigation
+  if (index === 0) {
+    //To handles sliders for Circle A
+    rangeSliderForNodes(circle_svg, circle_config);
+
+    //To add click event for all Nodes to navigate for Circle A
+    addClickEventsforNodes(circle_config);
+  }
+});
